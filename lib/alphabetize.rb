6aa6f18dc9c4462fdc7aed2ab4a895dfd157cd64
Leizl Samano <leizl.samano@gmail.com>
2018-12-06 23:25:14 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+# Each word is broken into an array of letters and then each letter is
+# converted into the index number of its letter in the Esperanto alphabet.
+# It then rearranges the words in arr based on index number.
 def alphabetize(arr)
-  # code here
+  arr.sort_by do |word|
+    word.chars.map { |letter| ESPERANTO_ALPHABET.index(letter) }
+  end
 end
+
+# See http://tylermachen.github.io/2015/08/10/ruby-tips-sort-methods.html#custom-alphabet
